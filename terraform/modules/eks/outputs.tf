7c86c97426ee1f11a6b6a1b051dd9874b53b2222
rajbirari800 <rajbirari800@gmail.com>
2026-06-12 03:21:08 +0530

    fix: update eks cluster security group output for rds connection

--- terraform/modules/eks/outputs.tf
+++ terraform/modules/eks/outputs.tf
@@ -1,4 +1,4 @@
 output "cluster_name" { value = aws_eks_cluster.main.name }
 output "cluster_endpoint" { value = aws_eks_cluster.main.endpoint }
-output "cluster_security_group_id" { value = aws_security_group.eks.id }
+output "cluster_security_group_id" { value = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id }
 output "oidc_provider" { value = aws_iam_openid_connect_provider.eks.arn }
